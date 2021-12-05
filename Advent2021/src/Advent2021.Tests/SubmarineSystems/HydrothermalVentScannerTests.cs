545da31ae31ac73f2c55433d0319e01b1e7a12f5
Tim White <twhite@conga.com>
2021-12-05 10:02:33 -0700

    day 5: part 1 got logic working for scanner

---
+++ Advent2021/src/Advent2021.Tests/SubmarineSystems/HydrothermalVentScannerTests.cs
@@ -0,0 +1,30 @@
+using Advent2021.Data;
+using Advent2021.Data.Models;
+using Advent2021.Data.Tests;
+using Advent2021.SubmarineSystems;
+
+namespace Advent2021.Tests.SubmarineSystems;
+
+public class HydrothermalVentScannerTests
+{
+    private readonly ILogger<HydrothermalVentScanner> _logger;
+
+    private readonly List<CoordinateLine> _fakeCoordinateLines;
+
+    public HydrothermalVentScannerTests()
+    {
+        _logger = Substitute.For<ILogger<HydrothermalVentScanner>>();
+        var parser = new RawDataService();
+        _fakeCoordinateLines = parser.ParseRawHydrothermicData(RawDataServiceTests.FakeHydrothermicVentsData);
+    }
+
+    [Fact]
+    public void ScanForDangerousOverlaps_Scans_For_Overlaps()
+    {
+        var subject = new HydrothermalVentScanner(_logger);
+
+        var actual = subject.ScanForDangerousOverlaps(_fakeCoordinateLines);
+
+        actual.Should().Be(5);
+    }
+}
--- Advent2021/src/Advent2021/SubmarineSystems/HydrothermalVentScanner.cs
+++ Advent2021/src/Advent2021/SubmarineSystems/HydrothermalVentScanner.cs
@@ -1,3 +1,5 @@
+using Advent2021.Data.Models;
+
 namespace Advent2021.SubmarineSystems;
 
 public class HydrothermalVentScanner
@@ -9,10 +11,59 @@ public class HydrothermalVentScanner
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    public int ScanForDangerousOverlaps()
+    public int ScanForDangerousOverlaps(List<CoordinateLine> coordinateLines)
     {
-        var answer = 0;
+        var coordinateOverlapMap = GetOverlappingCoordinates(coordinateLines);
+        var answer = coordinateOverlapMap.Count(c => c.Value > 1);
         _logger.LogCritical("At how many points do at least two lines overlap? Answer: {Answer}", answer);
         return answer;
+    }
+
+    private static Dictionary<Coordinate, int> GetOverlappingCoordinates(List<CoordinateLine> coordinateLines)
+    {
+        var coordinateOverlapMap = new Dictionary<Coordinate, int>();
+        foreach (var coordinateLine in coordinateLines)
+        {
+            var lineCoordinates = GetLineCoordinates(coordinateLine);
+            foreach (var coordinate in lineCoordinates)
+            {
+                var key = coordinateOverlapMap.Keys.FirstOrDefault(c => c.X == coordinate.X && c.Y == coordinate.Y);
+                if (key != null)
+                {
+                    coordinateOverlapMap[key]++;
+                }
+                else
+                {
+                    coordinateOverlapMap.Add(coordinate, 1);
+                }
+            }
+        }
+
+        return coordinateOverlapMap;
+    }
+
+    private static List<Coordinate> GetLineCoordinates(CoordinateLine coordinateLine)
+    {
+        var coordinates = new List<Coordinate>();
+        if (coordinateLine.Start.X == coordinateLine.End.X)
+        {
+            var start = Math.Min(coordinateLine.Start.Y, coordinateLine.End.Y);
+            var end = Math.Max(coordinateLine.Start.Y, coordinateLine.End.Y);
+            for (var i = start; i <= end; i++)
+            {
+                coordinates.Add(new Coordinate { X = coordinateLine.Start.X, Y = i });
+            }
+        }
+        else if (coordinateLine.Start.Y == coordinateLine.End.Y)
+        {
+            var start = Math.Min(coordinateLine.Start.X, coordinateLine.End.X);
+            var end = Math.Max(coordinateLine.Start.X, coordinateLine.End.X);
+            for (var i = start; i <= end; i++)
+            {
+                coordinates.Add(new Coordinate { X = i, Y = coordinateLine.Start.Y });
+            }
+        }
+
+        return coordinates;
     }
 }
